7a8162a8c46092855e9a025848ac9ee73a2899c5
stefian <v_ianta@yahoo.com>
2020-09-11 09:39:27 -0300

    + Added AWS End User Computing and Game Tech service icons to Ops Post and Dev Steps components

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -4,7 +4,7 @@
     <div class="card" v-for="(post,i) in matrix" :key="i">
       <div
         class="poster"
-        :style="{ backgroundImage: 'url(' + require('./../assets/img/cloud/' + randomInt(100,736) + '.png') + ')', 
+        :style="{ backgroundImage: 'url(' + require('./../assets/img/cloud/' + randomInt(100,745) + '.png') + ')', 
                   backgroundColor: 'rgba(' 
                     + randomInt(10,250) + ','
                     + randomInt(10,250) + ',' 
