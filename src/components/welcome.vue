19384f51d475bc76a93869b692b09b98dcc85f1b
stefian <v_ianta@yahoo.com>
2020-09-12 15:31:00 -0300

    + <p>serverless cloud Data Architect & monetization engine</p>

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="welcome">
     <h1 contenteditable="true">cloud DA @ AI speed</h1>
-    <p>serverless Cloud Data Architect and AI monetization engine</p>
+    <p>serverless cloud Data Architect & monetization engine</p>
     <!-- <p>evolutionary learning & fair monetary engine</p> -->
     <div class="cta">
       <router-link to="/signup">Sign Up</router-link>
