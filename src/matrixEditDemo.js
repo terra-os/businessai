3a88731f9e7ee905fbc13ca308c40ad52e2ec929
stefian <v_ianta@yahoo.com>
2020-09-17 12:25:10 -0300

    + matrixEdit updates

--- src/matrixEditDemo.js
+++ src/matrixEditDemo.js
@@ -1652,7 +1652,39 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
 ---`,
     
       ], [
-        
+        `# Implementing Ticket Updated Listener
+
+- 
+`,`orders/src/events/listeners/ticket-updated-listener.ts
+---
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketUpdatedEvent } from "@w3ai/common";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketUpdatedListener extends Listener<
+  TicketUpdatedEvent
+> {
+  readonly subject = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    const { title, price } = data;
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
+
+---`,
+    
       ], [
         
       ], [
