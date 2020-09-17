3c5addd87c44598dfdc81fe453ba3a741fe9666d
stefian <v_ianta@yahoo.com>
2020-09-17 12:36:18 -0300

    + # Testing OCC

--- src/matrixEditDemo.js
+++ src/matrixEditDemo.js
@@ -1877,7 +1877,68 @@ it("implements optimistic concurrency control", async () => {});
 ---`,
     
       ], [
-        
+        `# Testing OCC
+
+- $ tickets % npm run test
+`,`ticket.test.ts
+---
+import { Ticket } from "../ticket";
+
+it("implements optimistic concurrency control", async (done) => {
+  // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: "service",
+    price: 5,
+    userId: "123",
+  });
+
+  // Save the ticket to the database
+  await ticket.save();
+
+  // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // make two separate changes to the tickets
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
+  // save the first fetched ticket
+  await firstInstance!.save();
+
+  // save the second fetched ticket and expect an error - due to an outdated version property
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return done();
+  }
+
+  throw new Error("Should not reach this point");
+});
+
+
+---`,`
+---
+import { Ticket } from "../ticket";
+
+it("implements optimistic concurrency control", async () => {
+  // Create an instance of a ticket
+
+  // Save the ticket to the database
+
+  // Fetch the ticket twice
+
+  // make two separate changes to the tickets
+
+  // save the first fetched ticket
+
+  // save the second fetched ticket and expect an error
+  // - will have an outdated version nr
+
+});
+
+---`,`=IMAGE("https://storage.googleapis.com/ilabs/screens/screen%2094.png")`,`=IMAGE("https://storage.googleapis.com/ilabs/screens/screen%2093.png")`,
+    
       ], [
         
       ], [
