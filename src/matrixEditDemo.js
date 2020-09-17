1698f3d8ff0455cfb79adb9ddf00f5d5e7e2b823
stefian <v_ianta@yahoo.com>
2020-09-17 12:37:42 -0300

    + matrixEdit: matrixEdit()[7]

--- src/matrixEditDemo.js
+++ src/matrixEditDemo.js
@@ -1952,7 +1952,28 @@ it("implements optimistic concurrency control", async () => {
     ],
     [
       [
+        `# Testing version increments on multiple saves
 
+- 
+`,`ticket.test.ts
+---
+it("increments the version number on multiple saves", async () => {
+  const ticket = Ticket.build({
+    title: "service",
+    price: 20,
+    userId: "123",
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
+
+---`,
+    
       ], [
         
       ], [
