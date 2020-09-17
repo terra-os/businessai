231b82a9dee9e76b3a66dc833582b340cd66beaa
stefian <v_ianta@yahoo.com>
2020-09-17 13:06:40 -0300

    + Complete Listener Test

--- src/matrixEditDemo.js
+++ src/matrixEditDemo.js
@@ -2322,7 +2322,67 @@ it('acks the message', async () => {
 ---`,
     
       ], [
-        
+        `# A Complete Listener Test
+
+- 
+it("creates and saves a ticket", async () => {
+  const { listener, data, msg } = await setup();
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure a ticket was created
+  const ticket = await Ticket.findById(data.id);
+
+  expect(ticket).toBeDefined();
+  expect(ticket!.title).toEqual(data.title);
+  expect(ticket!.price).toEqual(data.price);
+});
+`,`ticket-created-listener.test.ts
+---
+import { Message } from "node-nats-streaming";
+import mongoose from "mongoose";
+import { TicketCreatedEvent } from "@w3ai/common";
+import { TicketCreatedListener } from "../ticket-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new TicketCreatedListener(natsWrapper.client);
+
+  // create a fake data event
+  const data: TicketCreatedEvent["data"] = {
+    version: 0,
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "service",
+    price: 10,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+
+  // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(), // a jest mock function to keep track of nr calls and params provided
+  };
+
+  return { listener, data, msg };
+};
+---`,`ticket-created-listener.test.ts
+---
+it("creates and saves a ticket", async () => {
+  const { listener, data, msg } = await setup();
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure a ticket was created
+  const ticket = await Ticket.findById(data.id);
+
+  expect(ticket).toBeDefined();
+  expect(ticket!.title).toEqual(data.title);
+  expect(ticket!.price).toEqual(data.price);
+});
+---`,`=IMAGE("https://storage.googleapis.com/ilabs/screens/screen%20119.png")`,
+    
       ], [
         
       ], [
