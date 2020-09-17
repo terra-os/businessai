0f987bf3070a74389cd8153de3bbfe17030ef9b8
stefian <v_ianta@yahoo.com>
2020-09-17 15:49:15 -0300

    + # Setup() for Testing Reservation

--- src/matrixEditDemo.js
+++ src/matrixEditDemo.js
@@ -2720,7 +2720,53 @@ export class OrderCreatedListener extends Listener<
 ---`,
     
       ], [
-        
+        `# Setup() for Testing Reservation
+
+- 
+`,`order-created-listener.test.ts
+---
+import { Message } from "node-nats-streaming";
+import mongoose from "mongoose";
+import { OrderCreatedEvent, OrderStatus } from "@w3ai/common";
+import { OrderCreatedListener } from "../order-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  // Create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    title: "service",
+    price: 99,
+    userId: "asdf",
+  });
+  await ticket.save();
+
+  // Create the fake data event
+  const data: OrderCreatedEvent["data"] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: "alskdfj",
+    expiresAt: "alskdjf",
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
+
+---`,
+    
       ]
 
     ]
