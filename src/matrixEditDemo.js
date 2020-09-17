d21749124924cc8651f10f2a7cea0bc46e1d0d00
stefian <v_ianta@yahoo.com>
2020-09-17 13:08:55 -0300

    + # Testing the Ticket Updated Listener

--- src/matrixEditDemo.js
+++ src/matrixEditDemo.js
@@ -2430,7 +2430,63 @@ orders.tickets
       [
 
       ], [
-        
+        `# Testing the Ticket Updated Listener 
+
+- 
+`,`ticket-updated-listener.test.ts
+---
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { TicketUpdatedEvent } from "@w3ai/common";
+import { TicketUpdatedListener } from "../ticket-updated-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  // Create a listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: "service",
+    price: 20,
+  });
+  await ticket.save();
+
+  // Create a fake data object
+  const data: TicketUpdatedEvent["data"] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: "new service",
+    price: 999,
+    userId: "whateverId",
+  };
+
+  // Create a fake msg object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  // return all of this stuff
+  return { msg, data, ticket, listener };
+};
+---`,`ticket-updated-listener.test.ts
+---
+const setup = async () => {
+  // Create a listener
+  // Create and save a ticket
+  // Create a fake data object
+  // Create a fake msg object
+  // return all of this stuff
+};
+
+it("finds, updates, and saves a ticket", async () => {});
+
+it("acks the message", async () => {});
+---`,
+    
       ], [
         
       ], [
