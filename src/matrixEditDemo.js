7692ee59e1c19d6d01f414aa3f3f66b646619539
stefian <v_ianta@yahoo.com>
2020-09-17 19:07:08 -0300

    + # Testing the Expiration Complete Listener / Orders Service

--- src/matrixEditDemo.js
+++ src/matrixEditDemo.js
@@ -1470,7 +1470,50 @@ it("emits an order cancelled event", async () => {
 ---`,
     
       ], [
-        
+        `# Testing the Expiration Complete Listener / Orders Service
+
+- the test setup function
+`,`expiration-complete-listener.test.ts
+---
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { OrderStatus, ExpirationCompleteEvent } from "@w3ai/common";
+import { ExpirationCompleteListener } from "../expiration-complete-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Order } from "../../../models/order";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: "service",
+    price: 20,
+  });
+  await ticket.save();
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: "alskdf",
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, order, ticket, data, msg };
+};
+
+---`,
+    
       ], [
         
       ], [
