066eeb25ed56789a33db2c92498005cede739a27
stefian <v_ianta@yahoo.com>
2020-09-18 23:04:57 -0300

    + <router-link to="/">AI Terralog</router-link>

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">Terra Cloud DA</router-link>
+      <router-link to="/">AI Terralog</router-link>
     </div>
     <nav>
       <ul>
