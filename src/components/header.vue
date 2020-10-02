57fe8e05865b9496079f0929cbd81a5b5ec84f56
stefian <v_ianta@yahoo.com>
2020-10-02 10:56:03 -0300

    + <title>WinWin OS</title>

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">WinWin</router-link>
+      <router-link to="/">WinWin OS</router-link>
     </div>
     <nav>
       <ul>
